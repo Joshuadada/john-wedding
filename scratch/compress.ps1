4cfb35c241226aa0b091a29af16fd533ce49c6be
Josh <jdbabatunde98@gmail.com>
2026-09-14 15:31:59 +0100

    fix og image picture

--- scratch/compress.ps1
+++ scratch/compress.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Drawing
 
-$src = "c:\Users\DELL\Documents\React\john-wedding\public\og-image.jpg"
-$dest = "c:\Users\DELL\Documents\React\john-wedding\public\og-image-compressed.jpg"
+$src = "c:\Users\DELL\Documents\React\john-wedding\public\images\pencil-sketch.jpg"
+$dest = "c:\Users\DELL\Documents\React\john-wedding\public\images\pencil-sketch-og.jpg"
 
 $img = [System.Drawing.Image]::FromFile($src)
 $bmp = New-Object System.Drawing.Bitmap 800, 450
@@ -20,10 +20,9 @@ foreach ($c in $codecs) {
 }
 
 $ep = New-Object System.Drawing.Imaging.EncoderParameters 1
-$ep.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter([System.Drawing.Imaging.Encoder]::Quality, 70L)
+$ep.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter([System.Drawing.Imaging.Encoder]::Quality, 75L)
 
 $bmp.Save($dest, $jpegCodec, $ep)
 $bmp.Dispose()
 
-Move-Item -Force $dest $src
-Write-Host "Compression completed successfully!"
+Write-Host "Pencil sketch OG image compressed successfully!"
